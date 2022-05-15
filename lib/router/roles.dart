6830902797cf5874a9d198ce8c08bb35f42aceae
WalterBanda <wbanda@kabarak.ac.ke>
2022-05-15 22:00:12 +0300

    ☕ Navlink version 2

--- lib/router/roles.dart
+++ lib/router/roles.dart
@@ -1,6 +1,7 @@
 import 'package:client/core/routes/router.dart';
 import 'package:client/core/routes/routes.dart';
 import 'package:client/styles/icons/chap_chap_icons.dart';
+import 'package:client/styles/index.dart';
 import 'package:client/styles/ui/colors.dart';
 import 'package:flutter/material.dart';
 
@@ -62,71 +63,75 @@ class PageNavigator extends StatelessWidget {
       );
     }
 
+    Widget _profile() {
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(30),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: AppColors.bg.withOpacity(0.2),
+            ),
+            child: const Icon(ChapChap.user, size: 50),
+          ),
+          const SizedBox(height: 20),
+          const Text(
+            "Wade Warren",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 24,
+              fontFamily: "SF Pro Rounded",
+              fontWeight: FontWeight.w600,
+            ),
+          )
+        ],
+      );
+    }
+
     return Drawer(
       width: 270,
       child: Center(
-        child: SizedBox(
-          width: 230,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              navLink(),
-              const SizedBox(height: 15),
-              navLink(),
-              const SizedBox(height: 15),
-              navLink(),
-              const SizedBox(height: 15),
-              navLink(),
-              const SizedBox(height: 15),
-              navLink(),
-              const SizedBox(height: 40),
-              _logoutButton(_userLogout),
-            ],
-          ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            _profile(),
+            const SizedBox(height: 40),
+            navLink(icon: ChapChap.home, label: "Home", onPressed: () {}),
+            navLink(icon: ChapChap.admin, label: "Admin", onPressed: () {}),
+            navLink(icon: ChapChap.car, label: "Garage", onPressed: () {}),
+            navLink(icon: ChapChap.user, label: "Profile", onPressed: () {}),
+            navLink(icon: Icons.settings, label: "Settings", onPressed: () {}),
+            const SizedBox(height: 40),
+            _logoutButton(_userLogout),
+          ],
         ),
       ),
     );
   }
 
-  Widget navLink({IconData? icon = ChapChap.info}) {
+  Widget navLink({
+    IconData? icon = ChapChap.info,
+    String label = "Link",
+    required VoidCallback onPressed,
+  }) {
     return TextButton(
-      onPressed: () {},
-      // style: TextButton.styleFrom(
-      //   primary: AppColors.primary,
-      // ),
-      style: ButtonStyle(
-        foregroundColor: MaterialStateProperty.resolveWith<Color?>(
-          (states) {
-            if (states.contains(MaterialState.focused) ||
-                states.contains(MaterialState.pressed)) {
-              return Colors.amber;
-            }
-            if (states.contains(MaterialState.hovered)) {
-              return AppColors.bg;
-            }
-            return AppColors.primary;
-          },
+      onPressed: onPressed,
+      child: ListTile(
+        hoverColor: AppColors.success,
+        textColor: AppColors.primary,
+        iconColor: AppColors.primary,
+        contentPadding: EdgeInsets.symmetric(vertical: 18, horizontal: 60),
+        minLeadingWidth: 0,
+        leading: Icon(icon, size: 20),
+        title: Text(
+          label,
+          style: const TextStyle(
+            fontFamily: "SF Pro Rounded",
+            fontSize: 18,
+            fontWeight: FontWeight.w700,
+          ),
         ),
-        overlayColor: MaterialStateProperty.all<Color>(Colors.transparent),
-        // surfaceTintColor: MaterialStateProperty.all<Color>(AppColors.success),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Icon(
-            icon,
-            size: 30,
-          ),
-          const Text(
-            "Home",
-            style: const TextStyle(
-              fontFamily: "SF Pro Rounded",
-              fontSize: 22,
-              fontWeight: FontWeight.w700,
-            ),
-          ),
-          const SizedBox(width: 30),
-        ],
       ),
     );
   }
@@ -145,7 +150,6 @@ class PageAppBar extends StatelessWidget {
         margin: const EdgeInsets.symmetric(vertical: 12, horizontal: 10),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(20),
-          // color: AppColors.bg,
         ),
         child: Row(
           mainAxisSize: MainAxisSize.max,
