2f8cd226f85dfc9f0b3f38104ceb0808f5f0c90f
WalterBanda <wbanda@kabarak.ac.ke>
2022-05-29 08:32:30 +0300

    ☕ Home layout done

--- lib/screens/roles/user/home.dart
+++ lib/screens/roles/user/home.dart
@@ -1,6 +1,11 @@
+import 'package:client/core/routes/routes.dart';
+import 'package:client/styles/icons/chap_chap_icons.dart';
+import 'package:client/styles/ui/colors.dart';
 import 'package:flutter/material.dart';
 
+import '../../../core/routes/router.dart';
 import '../../../router/roles.dart';
+import '../../auth/login.dart';
 
 class UserHome extends StatelessWidget {
   const UserHome({Key? key}) : super(key: key);
@@ -10,9 +15,47 @@ class UserHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Stack(
-      fit: StackFit.expand,
       children: [
-        MapLayer(),
+        const MapLayer(),
+        Positioned(
+          height: 100,
+          bottom: 30,
+          left: 10,
+          right: 10,
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 22),
+            width: 300,
+            decoration: const BoxDecoration(
+              color: AppColors.bgDark,
+              borderRadius: BorderRadius.all(
+                Radius.circular(20),
+              ),
+            ),
+            child: TextField(
+              onTap: () {
+                showDialog(
+                    useRootNavigator: false,
+                    context: context,
+                    builder: (context) => SearchOverlay());
+              },
+              style: const TextStyle(
+                fontFamily: "SF Pro Rounded",
+                fontSize: 15,
+                fontWeight: FontWeight.w600,
+              ),
+              decoration: InputDecoration(
+                filled: true,
+                prefixIcon: const Icon(ChapChap.search_filled),
+                fillColor: AppColors.input,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                  borderSide: BorderSide.none,
+                ),
+                hintText: "Looking for a Garage",
+              ),
+            ),
+          ),
+        ),
       ],
     );
   }
@@ -34,5 +77,14 @@ class _MapLayerState extends State<MapLayer> {
         child: Text("Unable to load a map, \n Please refresh"),
       ),
     );
+  }
+}
+
+class SearchOverlay extends StatelessWidget {
+  const SearchOverlay({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog();
   }
 }
