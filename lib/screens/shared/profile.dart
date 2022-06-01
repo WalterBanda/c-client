6dc9b661a5372059c0916bee12359fc3f2d8016f
WalterBanda <wbanda@kabarak.ac.ke>
2022-06-01 13:33:45 +0300

    ☕ Profile UI skeleton done

--- lib/screens/shared/profile.dart
+++ lib/screens/shared/profile.dart
@@ -14,51 +14,71 @@ class ProfilePage extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           children: [
             SingleChildScrollView(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(30),
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: AppColors.bg.withOpacity(0.2),
+              child: SizedBox(
+                width: 285,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(30),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: AppColors.bg.withOpacity(0.2),
+                      ),
+                      child: const Icon(ChapChap.user, size: 50),
                     ),
-                    child: const Icon(ChapChap.user, size: 50),
-                  ),
-                  const SizedBox(height: 40),
-                  const SizedBox(height: 20),
-                  const Text(
-                    "Fluff that up. I like to beat the brush. Be so very light. Be a gentle whisper. Let's make a nice big leafy tree. Once you learn the technique, ohhh! Turn you loose on the world; you become a tiger.",
-                    softWrap: true,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontFamily: "SF Pro Rounded",
-                      fontWeight: FontWeight.w400,
-                      fontSize: 16,
+                    const SizedBox(height: 40),
+                    const Text(
+                      "Username",
+                      softWrap: true,
+                      overflow: TextOverflow.ellipsis,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily: "SF Pro Rounded",
+                        fontWeight: FontWeight.w500,
+                        fontSize: 24,
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 20),
-                  const Text(
-                    "Your Activity",
-                    style: TextStyle(
-                      fontFamily: "SF Pro Rounded",
-                      fontWeight: FontWeight.w500,
-                      fontSize: 18,
+                    const SizedBox(height: 20),
+                    const Text(
+                      "Currently you have no description about you, add your description about you so that other people can know about you",
+                      softWrap: true,
+                      textAlign: TextAlign.center,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 5,
+                      style: TextStyle(
+                        fontFamily: "SF Pro Rounded",
+                        fontWeight: FontWeight.w400,
+                        fontSize: 16,
+                        color: AppColors.textPrimary,
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 20),
-                  const Text(
-                    "No Activity Currently, Book a garage and your activity will be shown here",
-                    softWrap: true,
-                    style: TextStyle(
-                      fontFamily: "SF Pro Rounded",
-                      fontWeight: FontWeight.w400,
-                      fontSize: 16,
+                    const SizedBox(height: 20),
+                    const Text(
+                      "Your Activity",
+                      style: TextStyle(
+                        fontFamily: "SF Pro Rounded",
+                        fontWeight: FontWeight.w500,
+                        fontSize: 18,
+                      ),
                     ),
-                  ),
-                ],
+                    const SizedBox(height: 20),
+                    const Text(
+                      "No Activity Currently, Book a garage and your activity will be shown here",
+                      softWrap: true,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily: "SF Pro Rounded",
+                        fontWeight: FontWeight.w400,
+                        fontSize: 16,
+                        color: AppColors.textPrimary,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
+            const SizedBox(height: 25),
             ElevatedButton(
               onPressed: () {},
               style: ElevatedButton.styleFrom(
