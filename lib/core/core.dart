416ce4eb04391e6b4ed2c40a87947a76c97e039e
WalterBanda <wbanda@kabarak.ac.ke>
2022-06-04 11:36:52 +0300

    🐛 Added App title

--- lib/core/core.dart
+++ lib/core/core.dart
@@ -10,6 +10,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "ChapChap",
       useInheritedMediaQuery: true,
       locale: DevicePreview.locale(context),
       builder: DevicePreview.appBuilder,
