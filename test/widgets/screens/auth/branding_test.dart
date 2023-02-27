19eaf8f457d4797e9cc6fb629e93df72b1242f17
Walter Banda <walterkaimurima@gmail.com>
2023-02-28 00:27:56 +0300

    🧪 Initial tests for Logo widget

---
+++ test/widgets/screens/auth/branding_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Splash screen tests', () {
+    testWidgets('Test if splash screen show the respective UI element',
+        (widgetTester) {});
+    testWidgets('Test splash screen element dimensions', (widgetTester) {});
+  });
+}
