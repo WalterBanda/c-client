1ee63f45d4cde24ed3241f3724e08834054ff6d1
Walter Banda <walterkaimurima@gmail.com>
2023-02-27 15:45:04 +0300

    ☕ Updated icon lists

--- lib/styles/icons/chapchap.dart
+++ lib/styles/icons/chapchap.dart
@@ -10,7 +10,7 @@ class ChapChap {
   static const _kFontFam = 'ChapChap';
   static const String? _kFontPkg = null;
 
-  static const IconData upload =
+  static const IconData add =
       IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData admin =
       IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
@@ -20,40 +20,48 @@ class ChapChap {
       IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData dark =
       IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData direction =
+  static const IconData edit =
       IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData email =
       IconData(0xe806, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData gain =
       IconData(0xe807, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData garage =
+  static const IconData gain_arrow =
       IconData(0xe808, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData github =
+  static const IconData garage =
       IconData(0xe809, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData google =
+  static const IconData github =
       IconData(0xe80a, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData home =
+  static const IconData google =
       IconData(0xe80b, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData info =
+  static const IconData home =
       IconData(0xe80c, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData light =
+  static const IconData info =
       IconData(0xe80d, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData location =
+  static const IconData light =
       IconData(0xe80e, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData loss =
+  static const IconData location =
       IconData(0xe80f, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData pin =
+  static const IconData lock =
       IconData(0xe810, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData add =
+  static const IconData login =
       IconData(0xe811, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData search =
-      IconData(0xe812, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData settings =
-      IconData(0xe813, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData user =
-      IconData(0xe814, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData logo =
-      IconData(0xe815, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+      IconData(0xe812, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData loss =
+      IconData(0xe813, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData loss_arrow =
+      IconData(0xe814, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData menu =
+      IconData(0xe815, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData phone =
       IconData(0xe816, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData search =
+      IconData(0xe817, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData settings =
+      IconData(0xe818, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData upload =
+      IconData(0xe819, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData user =
+      IconData(0xe81a, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
