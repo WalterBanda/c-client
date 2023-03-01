bbb5d67725bf0637450d89c967add43253a02b1d
Walter Banda <walterkaimurima@gmail.com>
2023-03-01 19:40:47 +0300

    🧪 Shared components initial tests

--- test/widgets/screens/auth/shared_test.dart
+++ test/widgets/screens/auth/shared_test.dart
@@ -1,9 +1,25 @@
+import 'package:client/styles/icons/chapchap.dart';
+import 'package:client/widgets/screens/auth/shared.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+import '../../../mocks/sandbox.dart';
 
 void main() {
   group('Splash screen tests', () {
     testWidgets('Test if splash screen show the respective UI element',
-        (widgetTester) {});
-    testWidgets('Test splash screen element dimensions', (widgetTester) {});
+        (tester) async {
+      await tester.pumpWidget(const Sandbox(testWidget: Logo()));
+
+// Checks respective widgets in tree
+      expect(find.text('ChapChap'), findsOneWidget);
+      expect(find.byIcon(ChapChap.logo), findsOneWidget);
+    });
+    testWidgets('Test splash screen element dimensions', (tester) async {
+      await tester.pumpWidget(const Sandbox(testWidget: Logo()));
+
+// Checks respective widgets in tree
+      expect(find.text('ChapChap'), findsOneWidget);
+      expect(find.byIcon(ChapChap.logo), findsOneWidget);
+    });
   });
 }
