91949200cfa13ef99ebc053d140ad7754e59f053
Walter Banda <walterkaimurima@gmail.com>
2023-03-07 20:00:41 +0300

    ☕ Provider Sandbox implementation

--- test/mocks/sandbox.dart
+++ test/mocks/sandbox.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
 
 class Sandbox extends StatelessWidget {
   final Widget testWidget;
@@ -10,6 +12,24 @@ class Sandbox extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: testWidget,
+    );
+  }
+}
+
+class ProviderSandbox extends StatelessWidget {
+  final List<SingleChildWidget> providers;
+  final Widget testWidget;
+
+  const ProviderSandbox(
+      {required this.providers, required this.testWidget, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: providers,
+      child: MaterialApp(
+        home: testWidget,
+      ),
     );
   }
 }
