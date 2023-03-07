04663a1a009b1e08e7968c3c74702de348caf3f0
Walter Banda <walterkaimurima@gmail.com>
2023-03-07 07:55:01 +0300

    📖 Description of Theme manager

--- lib/styles/theme/manager.dart
+++ lib/styles/theme/manager.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
+/// Responsible for switching between the various themes and theme modes in the application
 class ThemeManager extends ChangeNotifier {
-  ThemeMode _mode = ThemeMode.system;
+  /// This is responsible for switching between the various themes modes, has a default of [ThemeMode.system]
+  ThemeMode mode = ThemeMode.system;
 
-  ThemeMode get mode => _mode;
-
-  changeTheme(ThemeMode mode) {
-    _mode = mode;
+  /// This fn changes the theme across the application
+  changeTheme(ThemeMode thememode) {
+    thememode = thememode;
     notifyListeners();
   }
 }
