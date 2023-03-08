adedbcd23c060e77c8e357f4c8c2da00d2799361
Walter Banda <walterkaimurima@gmail.com>
2023-03-08 09:01:07 +0300

    🧪 Thememanager default mode check

---
+++ test/styles/theme/manager_test.dart
@@ -0,0 +1,24 @@
+import 'package:client/styles/theme/manager.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+
+import '../../mocks/sandbox.dart';
+
+void main() {
+  group("Theme manager tests", () {
+    testWidgets("Check if default Theme mode is system", (widgetTester) async {
+      await widgetTester.pumpWidget(ProviderSandbox(
+        providers: [
+          ChangeNotifierProvider(create: (context) => ThemeManager())
+        ],
+        testWidget: const Text("Hello"),
+      ));
+
+      final BuildContext context = widgetTester.element(find.byType(Text));
+
+      expect(Provider.of<ThemeManager>(context, listen: false).mode,
+          ThemeMode.system);
+    });
+  });
+}
