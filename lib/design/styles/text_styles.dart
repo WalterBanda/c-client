d554b303fe3746b2e6a1e12e6b68e30ab490afd0
Walter Banda <walterkaimurima@gmail.com>
2023-03-22 18:07:42 +0300

    ☕ Updated all text styles with base constant text style

--- lib/design/styles/text_styles.dart
+++ lib/design/styles/text_styles.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 
-class Fonts {
-  static String get title => "Roboto";
-}
-
 class FontSizes {
+  static const String fontFamily = "SF Pro Rounded";
   static double get display => 36;
   static double get title => 30;
   static double get heading => 24;
@@ -16,38 +13,42 @@ class FontSizes {
 
 /// Contains all typography styles for this app.
 class TextStyles {
+  /// Global TextStyle config
+  static TextStyle get base =>
+      const TextStyle(fontFamily: FontSizes.fontFamily);
+
   /// This is the style used for the branded name of app
   static TextStyle get display =>
-      TextStyle(fontSize: FontSizes.display, fontWeight: FontWeight.bold);
+      base.copyWith(fontSize: FontSizes.display, fontWeight: FontWeight.bold);
 
   /// Style used mostly for title of dialogs
   static TextStyle get title =>
-      TextStyle(fontSize: FontSizes.title, fontWeight: FontWeight.bold);
+      base.copyWith(fontSize: FontSizes.title, fontWeight: FontWeight.bold);
 
   /// Styles used mostly for `screen or page` headings
   static TextStyle get heading =>
-      TextStyle(fontSize: FontSizes.heading, fontWeight: FontWeight.w500);
+      base.copyWith(fontSize: FontSizes.heading, fontWeight: FontWeight.w500);
   static TextStyle get headingBold =>
       heading.copyWith(fontWeight: FontWeight.bold);
 
   /// Styles used for labels of form fields
   static TextStyle get label =>
-      TextStyle(fontSize: FontSizes.label, fontWeight: FontWeight.w600);
+      base.copyWith(fontSize: FontSizes.label, fontWeight: FontWeight.w600);
   static TextStyle get labelBold => label.copyWith(fontWeight: FontWeight.bold);
 
   /// Styles used for headings of sections
-  static TextStyle get headline => TextStyle(fontSize: FontSizes.headline);
+  static TextStyle get headline => base.copyWith(fontSize: FontSizes.headline);
   static TextStyle get headlineBold =>
       headline.copyWith(fontWeight: FontWeight.bold);
 
   /// General styles to be used in the body of app.
-  static TextStyle get body => TextStyle(fontSize: FontSizes.body);
+  static TextStyle get body => base.copyWith(fontSize: FontSizes.body);
   static TextStyle get bodySemibold =>
       body.copyWith(fontWeight: FontWeight.w600);
   static TextStyle get bodyBold => body.copyWith(fontWeight: FontWeight.bold);
 
   /// Used for captions of any emphasis texts.
-  static TextStyle get caption => TextStyle(fontSize: FontSizes.caption);
+  static TextStyle get caption => base.copyWith(fontSize: FontSizes.caption);
   static TextStyle get captionBold =>
       caption.copyWith(fontWeight: FontWeight.bold);
 }
