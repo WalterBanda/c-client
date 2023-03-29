7409dfff7717e173d1f9fb47c5f0fa7275c17cda
Walter Banda <walterkaimurima@gmail.com>
2023-03-29 11:34:23 +0300

    ☕ Better naming

--- lib/layout/auth.dart
+++ lib/layout/auth.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 // ignore: must_be_immutable
 class AuthLayout extends StatelessWidget {
   Widget illustration;
-  Widget navigator;
+  Widget page;
 
-  AuthLayout({required this.illustration, required this.navigator, super.key});
+  AuthLayout({required this.illustration, required this.page, super.key});
 
   List<Widget> children = [];
 
@@ -16,9 +16,9 @@ class AuthLayout extends StatelessWidget {
         children = constaints.maxWidth > 768
             ? [
                 Expanded(flex: 3, child: illustration),
-                Expanded(flex: 2, child: navigator)
+                Expanded(flex: 2, child: page)
               ]
-            : [Expanded(flex: 2, child: navigator)];
+            : [Expanded(flex: 2, child: page)];
 
         return Row(
           children: children,
