8412f2a4f9e2fa3de37221cd1d75bdabd8ee9f4b
Walter Banda <walterkaimurima@gmail.com>
2023-03-31 23:19:16 +0300

    ☕ Page fade transition for global pages

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -6,7 +6,17 @@ class AppRouter {
     switch (settings.name) {
       case AuthRouter.id:
         return PageRouteBuilder(
-            pageBuilder: (_, __, ___) => const AuthRouter());
+          pageBuilder: (_, __, ___) => const AuthRouter(),
+          transitionDuration: const Duration(milliseconds: 250),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            final curveTween = CurveTween(curve: Curves.decelerate);
+            Tween opacity = Tween(begin: 1, end: 0);
+            return FadeTransition(
+              opacity: animation.drive(opacity).drive(curveTween),
+              child: child,
+            );
+          },
+        );
       default:
         return PageRouteBuilder(
             pageBuilder: (_, __, ___) => const AuthRouter());
