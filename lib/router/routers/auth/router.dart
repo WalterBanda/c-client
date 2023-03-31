71773aa2bb7bdfbd32501a9e082f0e981f664228
Walter Banda <walterkaimurima@gmail.com>
2023-03-31 23:08:15 +0300

    ☕ Auth router scaffold

---
+++ lib/router/routers/auth/router.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class AuthRouter extends StatelessWidget {
+  const AuthRouter({super.key});
+
+  static const id = 'auth';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
