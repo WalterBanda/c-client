30433cf7a86d38faab23180946d5655ccd310555
Walter Banda <walterkaimurima@gmail.com>
2023-03-31 23:40:39 +0300

    ☕ Page router scaffold

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,18 +1,28 @@
-import 'package:client/animation/router/animations.dart';
-import 'package:client/router/routers/auth/router.dart';
 import 'package:flutter/material.dart';
+
+import 'routers/page/router.dart';
+import 'routers/auth/router.dart';
+import '../animation/router/animations.dart';
 
 class AppRouter {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case AuthRouter.id:
         return PageRouteBuilder(
-          pageBuilder: (_, __, ___) => const AuthRouter(),
-          transitionDuration: RouterAnimations.duration,
-          transitionsBuilder: RouterAnimations.fade,
-        );
+            transitionDuration: RouterAnimations.duration,
+            transitionsBuilder: RouterAnimations.fade,
+            pageBuilder: (_, __, ___) => const AuthRouter());
+
+      case PageRouter.id:
+        return PageRouteBuilder(
+            transitionDuration: RouterAnimations.duration,
+            transitionsBuilder: RouterAnimations.fade,
+            pageBuilder: (_, __, ___) => const AuthRouter());
+
       default:
         return PageRouteBuilder(
+            transitionDuration: RouterAnimations.duration,
+            transitionsBuilder: RouterAnimations.fade,
             pageBuilder: (_, __, ___) => const AuthRouter());
     }
   }
---
+++ lib/router/routers/page/router.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class PageRouter extends StatelessWidget {
+  const PageRouter({super.key});
+
+  static const id = 'app';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
