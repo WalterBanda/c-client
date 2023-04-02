94a07b01c4b7ed62dcfc012014597c78e60ffcb9
Walter Banda <walterkaimurima@gmail.com>
2023-04-02 16:49:13 +0300

    ☕ Reverted to the navigator 1.0 for routing
    # Why
    1. Easier routing logic
    2. Delegates path updates and state managment and restoration to underlying routes

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,75 +1,39 @@
+import 'package:client/screens/error/unknown.dart';
 import 'package:flutter/material.dart';
 
 import 'routers/page/router.dart';
 import 'routers/auth/router.dart';
 import '../animation/router.dart';
+import 'utils/route_parser.dart';
 
 class AppRouter {
   static GlobalKey<NavigatorState> router = GlobalKey();
-  static RouterDelegate<RouteSettings> delegate = _Delegate();
-  static RouteInformationParser<RouteSettings> parser = _Parser();
-}
-
-class AppRoutes {
-  static const String auth = AuthRouter.id;
-  static const String pages = PageRouter.id;
-}
-
-class _Parser extends RouteInformationParser<RouteSettings> {
-  // Url to navigation state
-  @override
-  Future<RouteSettings> parseRouteInformation(
-      RouteInformation routeInformation) async {
-    return RouteSettings(name: routeInformation.location ?? '/');
-  }
-
-  // Navigation state to url
-  @override
-  RouteInformation restoreRouteInformation(RouteSettings navigationState) {
-    return RouteInformation(location: '/${AppRoutes.auth}');
-  }
-}
-
-class _Delegate extends RouterDelegate<RouteSettings>
-    with ChangeNotifier, PopNavigatorRouterDelegateMixin<RouteSettings> {
-  @override
-  Widget build(BuildContext context) {
-    return Navigator(
-      key: navigatorKey,
-      onGenerateRoute: generateRoute,
-    );
-  }
-
-  Route<dynamic> generateRoute(RouteSettings settings) {
-    switch (settings.name) {
+  static Route<dynamic>? generateRoute(RouteSettings settings) {
+    switch (settings.name?.basePath) {
       case AppRoutes.auth:
         return PageRouteBuilder(
             transitionDuration: RouterAnimations.duration,
             transitionsBuilder: RouterAnimations.fade,
             pageBuilder: (_, __, ___) => const AuthRouter());
 
-      case AppRoutes.pages:
+      case AppRoutes.app:
         return PageRouteBuilder(
             transitionDuration: RouterAnimations.duration,
             transitionsBuilder: RouterAnimations.fade,
             pageBuilder: (_, __, ___) => const PageRouter());
-
-      default:
-        return PageRouteBuilder(
-            transitionDuration: RouterAnimations.duration,
-            transitionsBuilder: RouterAnimations.fade,
-            pageBuilder: (_, __, ___) => const AuthRouter());
     }
+    return null;
   }
 
-  @override
-  GlobalKey<NavigatorState>? get navigatorKey => AppRouter.router;
+  static Route<dynamic> unknownRoute(RouteSettings settings) {
+    return PageRouteBuilder(
+        transitionDuration: RouterAnimations.duration,
+        transitionsBuilder: RouterAnimations.fade,
+        pageBuilder: (_, __, ___) => const UnknownScreen());
+  }
+}
 
-  // Navigation state to app state
-  @override
-  Future<void> setNewRoutePath(RouteSettings navigationState) async {}
-
-  // App state to Navigation state, triggered by notifyListeners()
-  @override
-  RouteSettings get currentConfiguration => RouteSettings(name: '');
+class AppRoutes {
+  static const String auth = AuthRouter.id;
+  static const String app = PageRouter.id;
 }
