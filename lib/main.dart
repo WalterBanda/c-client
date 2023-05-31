6650f74275361faa250c3a756f6e9d870aa39a1a
Walter Banda <walterkaimurima@gmail.com>
2023-05-31 12:04:45 +0300

    🔧 Cleaning up unused dependacies

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,14 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'package:client/sandbox/nested_routing.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:provider/provider.dart';
 
 // 🏘️ Local imports
-import 'core/core.dart';
-import 'core/config/firebase_options.dart';
 import 'design/theme/manager.dart';
 
 void main() async {
   //  concrete binding for applications based on the Widgets framework
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Initialize Firebase
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
 
   setUrlStrategy(PathUrlStrategy());
 
@@ -26,7 +19,7 @@ void main() async {
           create: (context) => ThemeManager(),
         )
       ],
-      child: const App(),
+      child: const NestedRouterDemo(),
     ),
   );
 }
